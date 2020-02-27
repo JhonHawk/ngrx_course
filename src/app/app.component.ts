50d4b6146cf41a000f12c1d7113480dc9326d30a
Juan Manuel Martínez <bloodamn@gmail.com>
2020-02-26 23:55:32 -0600

    Before installation of ngrx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngrx-angular9';
+  contador: number;
+
+  constructor(){
+    this.contador = 10;
+  }
+
+  incrementar() {
+    this.contador ++;
+  }
+
+  decrementar(){
+    this.contador --;
+  }
 }
